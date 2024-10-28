0cbc3ceb87fdb6ae50741b3dfc0ce517b22cf8a1
Johan Kanflo <johan.kanflo@bitfuse.net>
2024-10-28 21:17:33 +0100

    Added help option to spinnder.py

--- spinner.py
+++ spinner.py
@@ -20,7 +20,15 @@ class MyApp(App):
         return container
 
 if __name__ == "__main__":
-    debug: bool = "-d" in sys.argv
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print(f"Usage: {sys.argv[0]} [-d] [-v] [-b]")
+        print(" -d  Set REMI log level to debug")
+        print(" -v  Set application log level to debug")
+        print(" -b  Open application in browser")
+        sys.exit(0)
+
+    remi_debug: bool = "-d" in sys.argv
+    app_debug: bool = "-d" in sys.argv
     fmt: str = "%(asctime)s %(name)-20s | %(message)s"
-    coloredlogs.install(level = logging.DEBUG if debug else logging.INFO, fmt=fmt)
-    start(MyApp, address = "127.0.0.1", port = 8888, start_browser = "-b" in sys.argv)
+    coloredlogs.install(level = logging.DEBUG if app_debug else logging.INFO, fmt=fmt)
+    start(MyApp, debug = remi_debug, address = "127.0.0.1", port = 8888, start_browser = "-b" in sys.argv)
